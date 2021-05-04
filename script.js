0d89eb61e739bc010044afa5d7742b40a8455cc1
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-04 13:02:53 +0530

    best way to close modal
    best way to close modal  by using function closemodal

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
+const fuckBody = document.querySelector("body");
 console.log(btnOpenModal);
 
 for (let i = 0; i < btnOpenModal.length; i++) {
@@ -18,9 +19,38 @@ for (let i = 0; i < btnOpenModal.length; i++) {
   });
 }
 
-document.querySelector(".close-modal").addEventListener("click", function () {
-  // document.querySelector(".hidden").style.display = "none";
-  modal.classList.add("hidden");
+//* long way to close the modal
+// document.querySelector(".close-modal").addEventListener("click", function () {
+// document.querySelector(".hidden").style.display = "none";
+//   modal.classList.add("hidden");
 
-  overlay.classList.add("hidden");
-});
+//   overlay.classList.add("hidden");
+// });
+
+//* short way to close the modal
+// btnCloseModal.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+
+//   overlay.classList.add("hidden");
+// });
+
+// overlay.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+
+//   overlay.classList.add("hidden");
+// });
+
+//! best way to close
+
+//? IN THIS I HAVE CREATED A FUNCTION  IN WHICH BY GIVING ANY ARUGEMENT EX.("closeModal(overlay);") THE MODAL WIIL CLOSE
+
+const closeModal = function (close) {
+  close.addEventListener("click", function () {
+    modal.classList.add("hidden");
+
+    overlay.classList.add("hidden");
+  });
+};
+
+closeModal(overlay);
+closeModal(btnCloseModal);
